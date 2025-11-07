1376c11d1c1d38faa22452561daa5f8f04aaa163
rcslbaa <ifasmarani3125@gmail.com>
2025-11-07 16:28:37 +0700

    membuat garis mendatar sebagai pemisah

--- app/src/main/java/com/example/questnavigasiui_015/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_015/view/Formulir.kt
@@ -54,3 +54,6 @@ fun FormIsian(
                 label = {Text(text = "Nama Lengkap")},
                 onValueChange = {},
             )
+            HorizontalDivider(modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp), thickness = Thickness,color = Color.Red)
