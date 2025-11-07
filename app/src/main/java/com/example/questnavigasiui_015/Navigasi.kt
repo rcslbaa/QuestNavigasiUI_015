e742d6f7b6b6843643f10bd166ad8e3485a3f5ae
rcslbaa <ifasmarani3125@gmail.com>
2025-11-07 21:09:31 +0700

    menyimpan daftar nama rute halaman

---
+++ app/src/main/java/com/example/questnavigasiui_015/Navigasi.kt
@@ -0,0 +1,19 @@
+package com.example.questnavigasiui_015
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigasiui_015.view.FormIsian
+import com.example.questnavigasiui_015.view.TampilData
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
