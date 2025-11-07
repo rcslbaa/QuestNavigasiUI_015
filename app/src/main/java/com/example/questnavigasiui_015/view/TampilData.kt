44bc2a3554deba23dd546c6196a11571c0cd82e0
rcslbaa <ifasmarani3125@gmail.com>
2025-11-07 19:58:31 +0700

    membuat daftar data

--- app/src/main/java/com/example/questnavigasiui_015/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_015/view/TampilData.kt
@@ -41,3 +41,13 @@ fun TampilData(
                 title = {Text(text = stringResource(id = R.string.tampil), color = Color.White)},
                 colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id=R.color.teal_700))
             )
+        }){ isiRuang ->
+        Column(modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween) {
+            Column(modifier = Modifier.padding(all = dimensionResource(id=R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy (space=dimensionResource(id = R.dimen.padding_small))) {
+                items.forEach { item ->
+                    Column {
+                        Text(text = item.first.uppercase(), fontSize = 16.sp)
+                        Text(text = item.second, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Cursive, fontSize = 22.sp)
+                    }
