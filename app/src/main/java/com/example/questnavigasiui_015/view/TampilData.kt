56bc821909bb0ec3cf60051306d02c99a0b8bb89
rcslbaa <ifasmarani3125@gmail.com>
2025-11-07 20:01:32 +0700

    membuat garis pemisah, lalu memberi jarak kosong, dan menambahkan tombol "Kembali"

--- app/src/main/java/com/example/questnavigasiui_015/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_015/view/TampilData.kt
@@ -51,3 +51,15 @@ fun TampilData(
                         Text(text = item.first.uppercase(), fontSize = 16.sp)
                         Text(text = item.second, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Cursive, fontSize = 22.sp)
                     }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                }
+                Spacer(modifier = Modifier.height(height = 10.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBackBtnClick) {
+                    Text(text = stringResource(id = R.string.back))
+                }
+            }
+        }
+    }
+}
