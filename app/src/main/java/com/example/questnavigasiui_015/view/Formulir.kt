8d6f94765652c149c220ee31f29d5058bf5fb704
rcslbaa <ifasmarani3125@gmail.com>
2025-11-07 15:56:25 +0700

    membuat kotak input teks untuk menulis Nama Lengkap

--- app/src/main/java/com/example/questnavigasiui_015/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_015/view/Formulir.kt
@@ -45,3 +45,12 @@ fun FormIsian(
         Column(modifier = Modifier.padding(paddingValues=isiRuang),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally) {
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(width = 250.dp),
+                label = {Text(text = "Nama Lengkap")},
+                onValueChange = {},
+            )
