2d3bf52eecc71f1f8277200bd698e7a210c5b622
rcslbaa <ifasmarani3125@gmail.com>
2025-11-07 21:14:13 +0700

    membuat tampilan utama aplikasi yang mengatur perpindahan antar layar

--- app/src/main/java/com/example/questnavigasiui_015/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_015/Navigasi.kt
@@ -17,3 +17,33 @@ enum class Navigasi {
     Formulirku,
     Detail
 }
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+) {
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        ) {
+            composable(route = Navigasi.Formulirku.name) {
+                FormIsian(
+                    //pilihanJK = JenisK.map { id -> konteks.resources.getString(id) },
+                    OnSubmitBtnClick = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name) {
+                TampilData(
+                    onBackBtnClick = {
+                        cancelAndBackToFormulir(navController)
+                    }
+                )
+            }
+        }
+    }
+}
